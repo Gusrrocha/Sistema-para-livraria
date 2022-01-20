400194b728c75a027b4383301f2fcf11d6384ca3
Gusrrocha <95317020+Gusrrocha@users.noreply.github.com>
2022-01-20 19:49:12 -0300

    Cliente dao

--- controller/cliente_page.py
+++ controller/cliente_page.py
@@ -5,6 +5,29 @@ class ClientePage(QWidget):
         uic.loadUi('view/cliente.ui', self)
 
         self.cancel_edit_btn.hide()
-        
+        self.remover_btn.clicked.connect(self.remover)
+        self.salvar_btn.clicked.connect(self.salvar)
+        self.painel_clientes.verticalHeader().setVisible(False)
+        self.painel_clientes.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.painel_clientes.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        self.cancel_edit.clicked.connect(self.stopEdit)
+        self.load()
+        self.painel_clientes.clicked.connect(self.cL)
+
+
+    def remover(self):
+        pass
+
+    def salvar(self):
+        pass
+
+    def stopEdit(self):
+        pass
+
+    def cL(self):
+        pass
+
+    def clear_(self):
+        pass
 
         
---
+++ model/cliente_dao.py
@@ -0,0 +1,60 @@
+import sqlite3
+from model import dbase
+from model.cliente import Cliente
+
+li = []
+def add(cliente):
+    li.append(cliente)
+    try:
+        conn = dbase.connect()
+        cursor = conn.cursor()
+        sql = """INSERT INTO Cliente (nome, telefone, endereco, cpf) VALUES (?, ?, ?, ?)"""
+        cursor.execute(sql, cliente.getCostumer())
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+def edit(cliente):
+    try:
+        conn = dbase.connect()
+        cursor = conn.cursor()
+        sql = """UPDATE Cliente SET nome=?, telefone=?, endereco=?, cpf=? WHERE id=?"""
+        l = cliente.getCostumer()
+        l.append(cliente.id)
+        cursor.execute(sql, l)
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+def removeC(id):
+    try:
+        conn = dbase.connect()
+        cursor = conn.cursor() 
+        sql = """DELETE FROM Cliente WHERE id=?"""
+        cursor.execute(sql, [id])
+        cursor.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
+def selectAll():
+    lis = []
+    try:
+        conn = dbase.connect()
+        cursor = conn.cursor()
+        sql = """SELECT * FROM Cliente ORDER BY upper(nome)"""
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for cliente in result:
+            new = Cliente(cliente[0], cliente[1], cliente[2], cliente[3], cliente[4])
+            lis.append(new)
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+    return lis
