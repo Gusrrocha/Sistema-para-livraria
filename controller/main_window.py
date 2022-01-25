b5912637fd591e25b9a3919ef49d9188bb177364
Gusrrocha <95317020+Gusrrocha@users.noreply.github.com>
2022-01-24 21:57:47 -0300

    Login

--- controller/main_window.py
+++ controller/main_window.py
@@ -5,13 +5,29 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('view/main_window.ui', self)
 
-        self.funcionario_label.setText(user_logged)
+        self.funcionario_label.setText(user_logged+"!")
         self.clientes_btn.clicked.connect(self.showCliente)
 
+        menu = QMenu()
+        self.mais_fun.setMenu(menu)
+        action_um = menu.addAction('Editar funcionário')
+        action_um.triggered.connect(self.edit_fun)
+        action_dois = menu.addAction('Sair')
+        action_dois.triggered.connect(self.sair)
 
-
+        
 
 
     def showCliente(self):
         self.tabela.insertWidget(1, ClientePage())
         self.tabela.setCurrentIndex(1)
+
+    def sair(self):
+        pass
+
+    def edit_fun(self):
+        pass
+        
+
+        
+        
